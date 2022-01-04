f9a963f92de587807692b389aa6383362b69c886
Aldo Balagulan <aldo.balagulan@gmail.com>
2022-01-05 01:36:13 +0800

    Result leaderboard now being read
    - The resulting leaderboard for results is now read and displayed.
    - Will have to write further code so that it is compiled into a list of
    dicts. Maybe use a class for writing more cars and drivers into the
    entrylist.

--- acc-entrylist-gen.py
+++ acc-entrylist-gen.py
@@ -1,5 +1,21 @@
-def main():
-	print("Hello world")
+#!/bin/python3
+
+import sys
+import json
+
+def main(args):
+	# Open the file
+	if (len(args) == 1):
+		sys.exit("You need to provide a valid JSON file.")
+	with open(args[1], mode="r", encoding="utf-8") as input_json:
+		race_data = json.load(input_json)
+		print("Data loaded:")
+		# print(json.dumps(json.loads(race_data).sessionResult.leaderBoardLines, sort_keys=True, indent=4))
+		leaderboard = race_data["sessionResult"]["leaderBoardLines"]
+		print(json.dumps(leaderboard, sort_keys=True, indent=2))
+		input_json.close()
+		sys.exit(0)
+
 
 if __name__ == "__main__":
-  main()
+	main(sys.argv)
