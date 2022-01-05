127264032d66b4ec0931e500e64cfe07c6293e86
Aldo Balagulan <aldo.balagulan@gmail.com>
2022-01-05 17:50:15 +0800

    Entrylist now being written
    - The entrylist JSON file is now being written by the script.
    - Finishing order is determined by the indices of the entires in the
    results leaderboard. Will need to verify if that is indeed the finishing
    order, but will have to test with multiple finishers.
    - Updated README file

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-ACC Entrylist Generator
+# ACC Entrylist Generator
 
 A util Python script for creating entrylists based on input JSON files (typically results).
+
+## Usage
+
+### Generate entrylist based on results leaderboard (for separate quali sessions, sprint qualifying)
+
+Simply run the Python script while supplying the path of the leaderboards/results JSON as the first argument.
+
+```
+./acc-entrylist-gen.py <your_results_file>.json
+```
+
+## Libraries
+
+Uses jsonpickle. Install it on Ubunutu with: `sudo apt install python3-jsonpickle`.
+
+## Planned features
+
+Reverse grid functionality
--- acc-entrylist-gen.py
+++ acc-entrylist-gen.py
@@ -2,6 +2,35 @@
 
 import sys
 import json
+import jsonpickle
+from datetime import datetime
+
+class Driver:
+	def __str__(self) -> str:
+		return ("Driver: " + self.firstName + " " + self.lastName + "\n" +
+			"SteamID: " + self.playerID)
+
+	def __init__(self, details):
+		self.firstName = details['firstName']
+		self.lastName = details['lastName']
+		self.shortName = details['shortName']
+		self.playerID = details['playerId']
+
+class EntrylistEntry:
+	def toJSON(self):
+		return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+	def __init__(self, drivers: Driver, gridPos):
+		self.drivers = drivers
+		self.forcedCarModel = -1
+		self.overrideDriverInfo = 0
+		self.defaultGridPosition = gridPos
+
+class Entrylist:
+	def __init__(self):
+		self.entries = []
+		self.forceEntryList = 1
 
 def main(args):
 	# Open the file
@@ -12,9 +41,39 @@ def main(args):
 		print("Data loaded:")
 		# print(json.dumps(json.loads(race_data).sessionResult.leaderBoardLines, sort_keys=True, indent=4))
 		leaderboard = race_data["sessionResult"]["leaderBoardLines"]
-		print(json.dumps(leaderboard, sort_keys=True, indent=2))
+		# print(json.dumps(leaderboard, sort_keys=True, indent=2))
+		print("cars:\n")
+
+		newEntryList = Entrylist()
+
+		for carIndex, carEntry in enumerate(leaderboard):
+			drivers = carEntry['car']['drivers']
+			print("Place: " + str(carIndex + 1))
+			print("Car Model: " + str(carEntry['car']['carModel']))
+
+			for driverIndex, driverEntry in enumerate(drivers):
+				print("Driver #" + str(driverIndex) + ":")
+				currDriver = Driver(driverEntry)
+				print(currDriver)
+
+			newEntryListEntry = EntrylistEntry(drivers, carIndex)
+			# print(json.dumps(newEntryListEntry, sort_keys=True, indent=2))
+			print(jsonpickle.encode(newEntryListEntry, make_refs=False, unpicklable=False))
+			newEntryList.entries.append(newEntryListEntry)
+			
+			print("\n===\n")
+
+		print("Finished entrylist:")
+		print(jsonpickle.encode(newEntryList, make_refs=False, unpicklable=False))
+
+		# Write file
+		filename = "entrylist_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".json"
+		with open(filename, 'w') as entrylistFile:
+			entrylistFile.write(jsonpickle.encode(newEntryList, make_refs=False, unpicklable=False))
+			entrylistFile.close()
+
 		input_json.close()
-		sys.exit(0)
+		sys.exit("Operation finished. New entrylist JSON file: " + filename)
 
 
 if __name__ == "__main__":
